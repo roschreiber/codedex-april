9225b293dc8badef2b39493cd509bca6c6e38cf1
roschreiber <schreiber.robin@icloud.com>
2024-04-29 14:34:36 +0200

    add credits
    Credits, change story

--- public/imports/typewriter.js
+++ public/imports/typewriter.js
@@ -5,7 +5,7 @@ let storyText = "This is .whiteÉ YOU./.\n.whiteÉ YOU./ are currently sitting i
 //Question 2
 storyText += "After the Issues from before, .whiteÉ YOU./ decide to hike through a .dgreenÏ FOREST./.\n.whiteÉ YOU./ suddenly stumble upon a mysterious, ancient .yellowÅ ARTIFACT./ half-buried in the ground.\nWhat do .whiteÉ YOU./ do?\qr"
 //Question 3
-storyText += ".whiteÉ YOU./ continue your hike through the .dgreenÏ FOREST./. Suddenly, you hear a rustling in a group of bushes nearby.\n .whiteÉ YOU./ quickly turn yourself to them, two glooming, .redÈ RED EYES./ stare back at .whiteÉ YOU./.\n.whiteÉ YOU./ ready yourself, thinking about what to do.";
+storyText += ".whiteÉ YOU./ continue your hike through the .dgreenÏ FOREST./. .whiteÉ YOU./ start to .yellowÍ WHISTLE./ some tunes.\n.whiteÉ YOU./ suddenly hear some rustling in a bush behind you. Seems like your whistling attracted.. something.\nYou quickly turn around to look at the bush and see some .redÈ EYES./ staring back at you.";
 storyText = storyText.replace(/\.lime-shadow/g, '<span class="lime-shadow">').replace(/\.red/g, '<span class="red">').replace(/\.yellow/g, '<span class="yellow">').replace(/\.white/g, '<span class="white">').replace(/\.dgreen/g, '<span class="dark-green">').replace(/\.\//g, '</span>');
 
 let sections = storyText.split("\qr");
@@ -20,7 +20,7 @@ let tally = personalities.reduce((acc, personality) => ({...acc, [personality]: 
 let answers = [
     {1: ".whiteI./ will rush outside to investigate the commotion and offer help, driven by curiosity and courage.", 2: ".whiteI./ quickly devise a plan to distract the .redÂ MIGHTY CAT./ and safely rescue the person, using my surroundings for that!", 3: ".whiteI./ envision a creative way to calm the .redÂ MIGHTY CAT./, trying to turn it into a .dgreenÂ PEACEFUL CAT./ instead!", 4: ".whiteI./ immediately step in to protect the elderly person, placing myself between her and the .redÂ MIGHTY CAT./."},
     {1: ".whiteI./ think .yellowIt./ looks shiny! Taking it for myself wouldn't hurt.", 2: ".whiteI./ think the best way to handle this without .redDAMAGING./ it would be calling someone who knows about this stuff?", 3: ".whiteI./ wonder about what stories this .yellowÅ ARTIFACT./ could tell. It's like finding a piece of a lost world!", 4: ".whiteI./ will leave it there, maybe hiding it under some more dirt to keep it .dgreenSAFE./."},
-    {1: ".whiteI./ think .yellowIt./ looks shiny! Taking it for myself wouldn't hurt.", 2: ".whiteI./ think the best way to handle this without .redDAMAGING./ it would be calling someone who knows about this stuff?", 3: ".whiteI./ wonder about what stories this .yellowÅ ARTIFACT./ could tell. It's like finding a piece of a lost world!", 4: ".whiteI./ will leave it there, maybe hiding it under some more dirt to keep it .dgreenSAFE./."},
+    {1: ".whiteI./ walk towards the bush, curious about who's checking out my awesome whistling skills. Hopefully it's a fan and not a .redcritic./!", 2: ".whiteI./ keep my cool and think about whether I should introduce myself or play it .dgreensafe./ and slowly back away.", 3: ".whiteI./ get .yellowexcited./, wondering if my music skills might have called forth a magical creature. Time for a .yellowduet./, perhaps?", 4:".whiteI./ decide that maybe whistling wasn't such a great idea. Time to make a .dgreenstrategic retreat./ and leave my audience wanting more."},
 ];
 
 answers = answers.map(answer => {
@@ -101,7 +101,7 @@ function displayAnswers() {
 
 function startQuiz() {
     window.addEventListener("keypress", handleKeypress);
-    typewriterEffect(sections[2], container, 'lime-shadow', displayAnswers);
+    typewriterEffect(sections[0], container, 'lime-shadow', displayAnswers);
 }
 
 startQuiz();
